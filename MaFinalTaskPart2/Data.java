039b7558180487ca3a81c991c70fa682c273e8b8
Nabora, Quinnie <79127396+nabora@users.noreply.github.com>
2022-05-13 11:40:15 +0800

    Create Data.java

---
+++ MaFinalTaskPart2/Data.java
@@ -0,0 +1,54 @@
+public class Data {
+	
+	private double h,b;
+	private double l;
+	private double w;
+	private double base;
+	private double height;
+	private double r;
+	
+	
+	
+		public void setHeight(double h) {
+			this.h = h;}
+		
+		public void setBase(double b) {
+			this.b = b;}
+	
+		public void setLenght(double l) {
+			this.l = l;}
+	
+		public void setWidth(double w) {
+			this.w = w;}
+	
+		public void setB(double base) {
+			this.base = base;}
+	
+		public void setH(double height) {
+			this.height = height;}
+	
+		public void setRaduis(double r) {
+			this.r = r;}
+	
+		public double getHeight() {
+			return h;}
+	
+		public double getBase() {
+			return b;}
+	
+		public double getLenght() {
+			return l;}
+	
+		public double getWidth() {
+			return w;}
+	
+		public double getB() {
+			return base;}
+	
+		public double getH() {
+			return height;}
+	
+		public double getRaduis() {
+			return r;}		
+
+}
