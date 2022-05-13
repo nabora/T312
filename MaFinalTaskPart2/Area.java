f033086b7a473cab0a4bddcf06d33e3477445866
Nabora, Quinnie <79127396+nabora@users.noreply.github.com>
2022-05-13 11:40:56 +0800

    Create Area.java

---
+++ MaFinalTaskPart2/Area.java
@@ -0,0 +1,23 @@
+class Area extends Data{
+	
+	
+	public void AoT(){
+		
+	
+			System.out.println("The Area of Triangle is ");}
+	
+	
+	public void AoR(){
+
+		 	System.out.println("The Area of Rectangle is ");}
+	
+	
+	public void AoP(){
+
+			System.out.println("The Area of Parallelogram is ");}
+		
+	
+	public void AoC(){
+	
+			System.out.println("The Area of Circle is ");}
+}
